ecd3779574ba418707c8db47f89e72c476325627
Asmarchess <asmaribrahimzade@gmail.com>
2024-04-25 10:19:26 +0300

    js2

---
+++ script.js
@@ -0,0 +1,24 @@
+let KreditCalc = (emekhaqqi, istenilenmebleg, istenilenmuddet) => {
+    const MAXMUDDET = 36
+    const MAXMEBLEG = emekhaqqi * 10
+    const MAXODENIS = emekhaqqi * 0.45
+
+    let faiz;
+    if (istenilenmuddet <= 12) {
+        faiz = 0.13;
+    } else if (istenilenmuddet <= 24) {
+        faiz = 0.15;
+    } else if (istenilenmuddet <= 36) {
+        faiz = 0.17;
+    }
+
+
+    let ayligOdenish = (istenilenmebleg + istenilenmebleg * faiz) / istenilenmuddet;
+    if (istenilenmuddet > MAXMUDDET || istenilenmebleg > MAXMEBLEG || ayligOdenish > MAXODENIS) {
+        return "Size bu shertlerle kredit dusmur";
+    } else {
+        return ` ${ istenilenmebleg } Azn mebleg size ${ faiz * 100 } % li ${ istenilenmuddet } ay muddetine aylig  ${ ayligOdenish } manat odenish verilir`;
+    }
+}
+
+console.log(KreditCalc(200, 600, 15));
